9d673ba96eab53ab385f1123d8819b4fa4719f3f
crackhopper <crackhopper@gmail.com>
2025-07-17 02:42:42 +0800

    调整一些配置，和ppt同步

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,14 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem"; // <-- 暂时注释掉
 import "@nomicfoundation/hardhat-ethers";
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
 import "hardhat-ethernal";
 import "hardhat-tracer";
 
 // 如果你需要 .env 文件中的常量，保留这行
 import * as dotenv from "dotenv";
 dotenv.config();
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28", // 你的 Solidity 版本
@@ -21,6 +22,7 @@ const config: HardhatUserConfig = {
   },
   tracer: {
     tasks: ["deploy", "run"],
+    gasCost: true,
   },
   ethernal: {
     disabled: false,
@@ -30,6 +32,21 @@ const config: HardhatUserConfig = {
     email: process.env.EMAIL,
     password: process.env.PASSWORD,
   },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS ? true : false, // 只在设置环境变量时启用
+    currency: "USD", // 显示美元估算成本
+    gasPriceApi:
+      "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice", // 获取实时 Gas Price，免费 5次/秒
+    //coinmarketcap: 'YOUR_COINMARKETCAP_API_KEY', // 获取 ETH 兑美元汇率
+    // outputFile: "gas-report.txt", // 将报告输出到文件
+    noColors: false, // 禁用颜色输出到文件
+    L1: "ethereum", // 为 L1 网络配置 （查询主网的gas price，结合当前gasUsed来评估费用）
+    // L2: 'optimism', // 如果你在 L2 上部署，可以配置 L2 网络
+    // includeIntrinsicGas: true, // 包含 21000 基础 Gas 和 calldata 开销
+  },
 };
+
+console.log(config);
+
 
 export default config;
